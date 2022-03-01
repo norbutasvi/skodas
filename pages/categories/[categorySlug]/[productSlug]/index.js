9425712e99534b3254b0110d8e03a3de86b9f033
norbutasvi <norbutasvilius@gmail.com>
2022-03-01 20:28:13 +0200

    test

--- pages/categories/[categorySlug]/[productSlug]/index.js
+++ pages/categories/[categorySlug]/[productSlug]/index.js
@@ -99,6 +99,15 @@ function Index({ product, header, footer, locale, array}) {
     return (
         <div>
     <Head>
+      <script>
+        {
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TNGVP2B')
+      }
+      </script>
         <title>{product[0].title}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
